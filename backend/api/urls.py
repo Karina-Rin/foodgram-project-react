f1922276ca769c8112f272bbf37c4d7af3300387
Карина Егорова <karishka.eo@yandex.ru>
2023-03-11 19:45:45 +0300

    32

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (CustomUserViewSet, FollowViewSet, IngredientViewSet,
-                       RecipeFavoriteViewSet, RecipeViewSet,
-                       ShoppingCartViewSet, TagViewSet)
+from api.views import (
+    CustomUserViewSet,
+    FollowViewSet,
+    IngredientViewSet,
+    RecipeFavoriteViewSet,
+    RecipeViewSet,
+    ShoppingCartViewSet,
+    TagViewSet,
+)
 
 router = DefaultRouter()
 router.register("users", CustomUserViewSet, basename="users")
@@ -12,9 +18,6 @@ router.register("recipes", RecipeFavoriteViewSet, basename="favorite")
 router.register("recipes", RecipeViewSet, basename="recipes")
 router.register("tags", TagViewSet, basename="tags")
 router.register("ingredients", IngredientViewSet, basename="ingredients")
-router.register(
-    r"users/(?P<user_id>\d+)/follows", FollowViewSet, basename="follows"
-)
 router.register(
     r"recipes/(?P<recipe_id>\d+)/favorite",
     RecipeFavoriteViewSet,
