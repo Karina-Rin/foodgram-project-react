2b418a04086c8d93be40084a6007d9a7dd1a6968
Карина Егорова <karishka.eo@yandex.ru>
2023-03-19 22:25:08 +0300

    users.models import User

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -4,6 +4,8 @@ from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
 from django.db.models import UniqueConstraint
 
+from users.models import User
+
 max_legth = settings.MAX_LEGTH
 
 User = get_user_model()
