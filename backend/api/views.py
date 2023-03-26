3332e0e95849605ba3d76716712c81a3cf3a0eba
Карина Егорова <karishka.eo@yandex.ru>
2023-03-26 19:09:06 +0300

    save

--- backend/api/views.py
+++ backend/api/views.py
@@ -106,28 +106,9 @@ class RecipeViewSet(ModelViewSet, AddDelViewMixin):
 
     def get_queryset(self):
         queryset = Recipe.objects.all()
-        user = self.request.user
 
         if not self.request.query_params:
             return queryset
-
-        is_favorited = self.request.query_params.get("is_favorited")
-        if is_favorited == "1":
-            favorited = Favorites.objects.filter(user=user)
-            queryset = queryset.filter(favorites__in=favorited)
-        if is_favorited == "0":
-            favorited = Favorites.objects.filter(user=user)
-            queryset = queryset.exclude(favorites__in=favorited)
-
-        is_in_shopping_cart = self.request.query_params.get(
-            "is_in_shopping_cart"
-        )
-        if is_in_shopping_cart == "1":
-            shopping_cart = Carts.objects.filter(user=user)
-            queryset = queryset.filter(carts__in=shopping_cart)
-        if is_in_shopping_cart == "0":
-            shopping_cart = Carts.objects.filter(user=user)
-            queryset = queryset.exclude(carts__in=shopping_cart)
 
         tags = self.request.query_params.getlist("tags")
         if tags is not None:
